91a6cafdb064b8e7f62f064dbd5c975865d4c41f
heidi konig <heidik1989@icloud.com>
2019-07-26 10:28:32 +0200

    Create Globalstyle file

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import GlobalStyles from "../app/Globalstyles"
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Outbreak</h1>
+    </>
+  )
+}
+
+export default App
