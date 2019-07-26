75a2e458b7f98defdefaa9347f2de5b740ff22a6
heidi konig <heidik1989@icloud.com>
2019-07-26 12:27:27 +0200

    Add headerimage to landingpage

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,8 +1,25 @@
 import React from "react"
 import styled from "styled-components"
 
+import headerImg from "../images/dream.png"
+
+const StyledHeader = styled.h1`
+  margin: 0;
+  text-align: center;
+  font-size: 1.5em;
+`
+
+const StyledImg = styled.img`
+  height: 100%;
+`
+
 function Landingpage() {
-  return <h1>Welcome to outbreak</h1>
+  return (
+    <>
+      <StyledImg className="headerImg" src={headerImg} alt="dream-image" />
+      <StyledHeader>Welcome to Outbreak</StyledHeader>
+    </>
+  )
 }
 
 export default Landingpage
