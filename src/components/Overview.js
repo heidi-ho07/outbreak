4fe0ed39fa9087a702bb52c65afe38522c7af452
heidi konig <heidik1989@icloud.com>
2019-07-28 18:15:24 +0200

    Style input and create countries array

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -25,13 +25,35 @@ const StyledHeader = styled.div`
   text-align: center;
 `
 
+const StyledInput = styled.input`
+  background: #fff;
+  border-bottom: 2px solid #d7d1c9;
+  border-radius: 2px;
+  border-top: 0;
+  border-right: 0;
+  border-left: 0;
+  margin-top: 10px;
+`
+
 function Overview() {
+  const [countries, setCountries] = React.useState(["Australien", "Thailand"])
+
   return (
     <>
       <StyledImg src={headerImg} alt="travel-diary image" />
       <StyledHeader>
         <StyledContent>Übersicht meiner Länder</StyledContent>
         <StyledIcon className="fas fa-folder-plus fa-lg" />
+        <br />
+        <form>
+          <StyledInput type="search" />
+          <button type="submit">
+            <i className="fa fa-search" />
+          </button>
+        </form>
+        {countries.map(country => {
+          return <p>{country}</p>
+        })}
       </StyledHeader>
     </>
   )
