8d5aba94002a7d109d4e48b9c0343310a71c8203
heidi konig <heidik1989@icloud.com>
2019-07-28 11:37:02 +0200

    Create overview file and add react router

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 import GlobalStyles from "../app/Globalstyles"
 import Header from "../components/Header"
@@ -9,7 +10,9 @@ function App() {
     <>
       <GlobalStyles />
       <Header />
-      <Landingpage />
+      <Router>
+        <Route path="/" component={Landingpage} />
+      </Router>
     </>
   )
 }
