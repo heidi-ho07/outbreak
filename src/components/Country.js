99fe7b76259afa5880ef92042258436b17afe3ed
heidi konig <heidik1989@icloud.com>
2019-07-30 17:04:27 +0200

    Create mock data

--- src/components/Country.js
+++ src/components/Country.js
@@ -10,8 +10,28 @@ const StyledImg = styled.img`
   margin-bottom: 20px;
 `
 
+const StyledContainer = styled.div`
+  text-align: center;
+`
+
+const StyledOverview = styled.div`
+  text-align: center;
+  font-size: 12px;
+`
+
 function Country() {
-  return <StyledImg src={headerImg} alt="travel-diary image" />
+  return (
+    <>
+      <StyledImg src={headerImg} alt="travel-diary image" />
+
+      <StyledOverview>Beiträge (3)</StyledOverview>
+      <StyledContainer>
+        <p>2019/07/01 - Bangkok</p>
+        <p>2019/07/07 - Chiang Mai</p>
+        <p>2019/07/15 - Koh Samui</p>
+      </StyledContainer>
+    </>
+  )
 }
 
 export default Country
