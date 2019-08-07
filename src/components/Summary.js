02f6ed846288b3cfac93571452591f30dd24b6e2
heidi konig <heidik1989@icloud.com>
2019-08-07 10:37:25 +0200

    Styling summary file

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -11,19 +11,33 @@ const StyledImg = styled.img`
 `
 const StyledText = styled.div`
   border: 2px solid #bbded6;
+  border-radius: 5px;
   padding: 20px;
+  margin: 15px;
 `
 
 const StyledHeadline = styled.h1`
-  color: gray;
+  color: white;
+`
+
+const StyledHeader = styled.div`
+  background-image: url(${headerImg});
+  background-size: 100%;
+  height: 142px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 50px;
+  filter: grayscale(20%);
 `
 
 function Summary(props) {
   return (
     <>
-      <StyledHeadline>{props.match.params.title}</StyledHeadline>
-      <StyledImg src={headerImg} alt="summary image" />
-      <h1>01. Sep. 2019 - Chang Mai</h1>
+      <StyledHeader>
+        <StyledHeadline>{props.match.params.title}</StyledHeadline>
+        {/* <StyledImg src={headerImg} alt="summary image" /> */}
+      </StyledHeader>
       <StyledText>
         Traveler traveler blogger, expedition expedition modern Travel cute
         wanderlust website. Cute modern organized, travelblogger modern
