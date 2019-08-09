cec74205c177916615997a59fcab5a74c2950447
heidi konig <heidik1989@icloud.com>
2019-08-09 13:45:34 +0200

    Create new landingpage

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import GlobalStyles from "../app/Globalstyles"
 import Header from "../components/Header"
 import Landingpage from "../components/Landingpage"
+import Home from "../components/Home"
 import Overview from "../components/Overview"
 import Country from "../components/Country"
 import Form from "../components/Form"
@@ -18,6 +19,7 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/" component={Landingpage} />
+          <Route exact path="/home" component={Home} />
           <Route exact path="/overview" component={Overview} />
           <Route exact path="/country/:name" component={Country} />
           <Route exact path="/form" component={Form} />
--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,75 +1,15 @@
 import React from "react"
 import styled from "styled-components"
-import { Link } from "react-router-dom"
-import PropTypes from "prop-types"
 
 import headerImg from "../images/dream.png"
-import Button from "./Button"
-
-const StyledHeader = styled.h1`
-  margin: 0;
-  text-align: center;
-  font-size: 1.5em;
-  letter-spacing: 0.5em;
-  line-height: 1.5;
-  color: #414141;
-`
-
-const StyledImg = styled.img`
-  height: 100%;
-  width: 100%;
-  margin-top: 20px;
-  margin-bottom: 30px;
-  box-shadow: 0 5px 15px -5px #00000070;
-`
-
-const StyledContent = styled.div`
-  font-size: 1em;
-  padding: 10px;
-  margin-bottom: 30px;
-  line-height: 1.5;
-  color: #414141;
-`
-const StyledContainer = styled.div`
-  padding: 18px;
-  text-align: center;
-`
-const StyledIconPen = styled.i`
-  color: white;
-  padding: 5px 5px 5px 20px;
-  margin-left: 15px;
-  border-left: 2px solid #bbded6;
-`
 
 function Landingpage() {
   return (
     <>
-      <StyledImg src={headerImg} alt="dream-image" />
-      <StyledContainer>
-        <StyledHeader>Welcome to Outbreak</StyledHeader>
-        <StyledContent>
-          Hallo ich bin Outbreak - dein Reisetagebuch.
-        </StyledContent>
-
-        <StyledContent>
-          Ich bin super gespannt auf deine Erlebnisse. Klicke auf den Button und
-          fange an mir von deinen Abenteuern zu erzählen.
-        </StyledContent>
-        <Link to="/overview">
-          <Button>
-            Reisetagebuch schreiben
-            <StyledIconPen className="fas fa-pencil-alt fa-lg" />
-          </Button>
-        </Link>
-      </StyledContainer>
+      <h1>I am the Landingpage</h1>
+      <i src={headerImg} alt="dream-image" />
     </>
   )
-}
-
-Landingpage.propTypes = {
-  icon: PropTypes.string,
-  className: PropTypes.string,
-  content: PropTypes.string
 }
 
 export default Landingpage
